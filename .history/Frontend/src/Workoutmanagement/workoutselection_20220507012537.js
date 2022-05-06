b093bfa6ea6a4f505cf08794acfbab7f6d204cdb
Z00481XT <leon.debatin@siemens.com>
2022-05-07 01:54:54 +0200

    wourkout addition weiter

--- .history/Frontend/src/Workoutmanagement/workoutselection_20220507012537.js
+++ .history/Frontend/src/Workoutmanagement/workoutselection_20220507012537.js
@@ -41,18 +41,16 @@ async init() {
 
     // Datensatz laden
     
-    //   this._url = `/exercise/${this._exerciseId}`;
-    //   this._dataset = await this._app.backend.fetch("GET", this._url);
-    //   this._title = `${this._dataset.name} ${this._dataset.image}`;
+      this._url = `/exercise/${this._exerciseId}`;
+      this._dataset = await this._app.backend.fetch("GET", this._url);
+      this._title = `${this._dataset.name} ${this._dataset.image}`;
     
       let { default: WorkoutAddition } = await import(
         "./workoutaddition.js"
       );
 
-      let Additor = new WorkoutAddition(this);
+      let page = new WorkoutAddition(this);
       
-      Additor.saveandreturnWorkoutAddition(this._exerciseId, this._app);
-
 
 }
 }
