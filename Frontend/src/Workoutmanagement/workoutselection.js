894cf25f784d7c2522468d40064e4efd7b94c0e3
Z00481XT <leon.debatin@siemens.com>
2022-05-07 16:09:41 +0200

    save exercise to workout zwischenschritt (yannick+leon)

--- Frontend/src/Workoutmanagement/workoutselection.js
+++ Frontend/src/Workoutmanagement/workoutselection.js
@@ -1,7 +1,7 @@
 "use strict";
 
 import Page from "../page.js";
-import HtmlTemplate from "./workout-selection.html";
+import HtmlTemplate from "./page-workouthinzufuegen.html";
 
 
 export default class WorkoutSelection extends Page {
@@ -41,17 +41,18 @@ async init() {
 
     // Datensatz laden
     
-    //   this._url = `/exercise/${this._exerciseId}`;
-    //   this._dataset = await this._app.backend.fetch("GET", this._url);
-    //   this._title = `${this._dataset.name} ${this._dataset.image}`;
+    this._url = `/exercise/${this._exerciseId}`;
+    this._dataset = await this._app.backend.fetch("GET", this._url);
+    //this._title = `${this._dataset.name} ${this._dataset.image}`;
     
       let { default: WorkoutAddition } = await import(
         "./workoutaddition.js"
       );
 
-      let Additor = new WorkoutAddition(this);
+
       
-      Additor.saveandreturnWorkoutAddition(this._exerciseId, this._app);
+      let Additor = new WorkoutAddition(this);
+      Additor.saveandreturnWorkoutAddition(this._dataset, this._app);
 
 
 }
