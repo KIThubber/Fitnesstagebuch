db548c847794849c49a0e968875142b0225d8b2e
JanHausdorf <lastf0rc3@gmail.com>
2022-05-08 12:00:21 +0200

    Anpassen Database Vorlagen

--- Backend/src/database.js
+++ Backend/src/database.js
@@ -29,44 +29,167 @@ class DatabaseFactory {
 
   async _createDemoExercises() {
     let exercises = this.database.collection("exercises");
-
+    // <p></p>
     if ((await exercises.estimatedDocumentCount()) === 0) {
       exercises.insertMany([
+        // dummy
         {
-          name: "Bankdrücken",
+          name: "",
           image: "Musterbild",
-          difficulty: "schwer",
+          difficulty: "",
+          muscleGroup: "",
+          description: "<p>1. </p><p>2. </p><p>3. </p><p>4.</p>",
+        },
+
+        {
+          name: "Bankdrücken mit Langhantel",
+          image: "Musterbild",
+          difficulty: "Mittel",
           muscleGroup: "Brust",
           description:
-            "Lege dich auf eine Flachbank. Positioniere deine Beine etwa schulterbreit auf dem Boden. Greife eine Langhantel etwas breiter als schulterbreit und fixiere deine Schultern. Drücke die Hantel.",
+            "<p>1. Legen Sie sich mit den Füßen auf dem Boden flach auf die Bank. Mit gestreckten Armen die Stange lösen.</p><p>2. Senken Sie die Stange auf Ihre mittlere Brust.</p><p>3. Heben Sie die Stange, bis Sie Ihre Ellbogen gesperrt haben.</p>",
+        },
+        {
+          name: "Schrägbankdrücken mit Langhantel",
+          image: "Musterbild",
+          difficulty: "Mittel",
+          muscleGroup: "Brust",
+          description:
+            "<p>1. Positionieren Sie die Bank zwischen 30 und 45 Grad.</p><p>2. Legen Sie sich mit den Füßen auf dem Boden flach auf die Bank. Mit gestreckten Armen die Stange lösen.</p><p>3. Senken Sie die Stange auf Ihre mittlere Brust</p><p>4. Heben Sie die Stange (langsam und kontrolliert) an, bis Sie Ihre Ellbogen gestreckt haben.</p>",
         },
         {
           name: "Kreuzheben",
           image: "Musterbild",
-          difficulty: "schwer",
+          difficulty: "Fortgeschritten",
           muscleGroup: "Rücken",
-          description: "Ziehe eine Langhantel.",
+          description: "<p>1. Stellen Sie sich mit dem Mittelfuß unter die Stange und greifen Sie die Stange mit den Händen etwa schulterbreit auseinander.</p><p>2. Beugen Sie Ihre Knie und heben Sie dann die Stange an, indem Sie Ihren Rücken strecken. Es ist wichtig, den Rücken gerade zu halten.</p><p>3. Stehen Sie zu Ihrer vollen Höhe und halten Sie.</p><p>4.Senken Sie die Stange auf den Boden, indem Sie Ihre Knie beugen und Ihren Rücken gerade halten.</p>",
+        },
+        {
+          name: "Langhantelrudern vorgebeugt",
+          image: "Musterbild",
+          difficulty: "Mittel",
+          muscleGroup: "Rücken",
+          description: "<p>1. Greifen Sie eine Langhantel mit einem schulterbreiten pronierten oder supinierten Griff.</p><p>2. Beuge dich an deinen Hüften nach vorne, während du einen flachen Rücken behältst.</p><p>3. Ziehen Sie das Gewicht in Richtung Oberbauch.</p><p>4. Senken Sie das Gewicht kontrolliert ab und wiederholen Sie den Vorgang.</p>",
+        },
+        {
+          name: "Squat",
+          image: "Musterbild",
+          difficulty: "Fortgeschritten",
+          muscleGroup: "Beine",
+          description: "<p>1. Stellen Sie sich mit den Füßen schulterbreit auseinander. Behalten Sie die natürliche Wölbung in Ihrem Rücken bei, drücken Sie Ihre Schulterblätter zusammen und heben Sie Ihre Brust an.</p><p>2. Greifen Sie die Stange über Ihre Schultern und stützen Sie sie auf Ihrem oberen Rücken ab. Lösen Sie die Stange, indem Sie Ihre Beine strecken, und treten Sie einen Schritt zurück.</p><p>3. Beugen Sie Ihre Knie, während Sie das Gewicht senken, ohne die Form Ihres Rückens zu verändern, bis sich Ihre Hüften unter Ihren Knien befinden.</p><p>4. Heben Sie die Stange zurück in die Ausgangsposition, heben Sie sie mit den Beinen an und atmen Sie oben aus.</p>",
+        },
+        {
+          name: "Wadenheben im Sitzen",
+          image: "Musterbild",
+          difficulty: "Leicht",
+          muscleGroup: "Beine",
+          description: "<p>1. Machen Sie es sich auf der Maschine bequem und legen Sie dann Ihre Unterschenkel unter den gepolsterten Hebel. Stellen Sie Ihre Zehen und Fußballen auf die Fußstützen.</p><p>2. Verhindern Sie, dass das Gewicht nach vorne rutscht, indem Sie die Griffe greifen, und lösen Sie den Sicherheitsbügel. Senken Sie das Gewicht, bis Ihre Waden gestreckt sind.</p><p>3.Drücken Sie Ihre Fersen nach oben, um den gepolsterten Hebel anzuheben und die kontrahierte Position zu halten, und senken Sie sich dann langsam wieder in die Ausgangsposition ab. Wiederholen.</p>",
+        },
+        {
+          name: "Ausfallschritt Kettlebell",
+          image: "Musterbild",
+          difficulty: "Leicht",
+          muscleGroup: "Beine",
+          description: "<p>1. Stellen Sie sich aufrecht hin, die Füße leicht auseinander und halten Sie eine Kettlebell in einer Hand.</p><p>2. Bringen Sie dasselbe Bein wie den Arm, der die Kettlebell hält, vor sich, gehen Sie in die Hocke, bis Ihr Oberschenkel parallel zum Boden ist, und halten Sie Ihren Rücken gerade.</p><p>3. Zurück in die Ausgangsposition und wiederholen.</p>",
+        },
+        {
+          name: "Beinpresse",
+          image: "Musterbild",
+          difficulty: "Mittel",
+          muscleGroup: "Beine",
+          description: "<p>1. Stellen Sie Ihre Beine schulterbreit auf die Plattform.</p><p>2. Lassen Sie das Gewicht los und strecken Sie Ihre Beine vollständig aus, ohne Ihre Knie zu sperren.</p><p>3. Senken Sie das Gewicht ab, bis sich Ihre Beine in einem 90°-Winkel befinden (aber lassen Sie NICHT zu, dass sich Ihr Gesäß und Ihr unterer Rücken vom Polster abheben. Dadurch wird Ihr unterer Rücken in eine runde Position gebracht, was sehr gefährlich ist.)</p><p>4. Drücken Sie das Gewicht zurück in die Ausgangsposition.</p>",
         },
       ]);
     }
   }
 
   async _createDemoWorkouts() {
-    let workouts = this.database.collection("workouts");
-    if ((await workouts.estimatedDocumentCount()) === 0) {
-      workouts.insertMany([
+    let exercises = this.database.collection("workouts");
+    if ((await exercises.estimatedDocumentCount()) === 0) {
+      exercises.insertMany([
         {
-          name: "Willy",
+          name: "Push",
           exercises: [
-            {
-              name: "Kreuzheben",
+            {              
+              //{
+                name: "Bankdrücken mit Langhantel",
+                image: "Musterbild",
+                difficulty: "Mittel",
+                muscleGroup: "Brust",
+                description:
+                  "<p>1. Legen Sie sich mit den Füßen auf dem Boden flach auf die Bank. Mit gestreckten Armen die Stange lösen.</p><p>2. Senken Sie die Stange auf Ihre mittlere Brust.</p><p>3. Heben Sie die Stange, bis Sie Ihre Ellbogen gesperrt haben.</p>",
+              },
+              {
+                name: "Schrägbankdrücken mit Langhantel",
+                image: "Musterbild",
+                difficulty: "Mittel",
+                muscleGroup: "Brust",
+                description:
+                  "<p>1. Positionieren Sie die Bank zwischen 30 und 45 Grad.</p><p>2. Legen Sie sich mit den Füßen auf dem Boden flach auf die Bank. Mit gestreckten Armen die Stange lösen.</p><p>3. Senken Sie die Stange auf Ihre mittlere Brust</p><p>4. Heben Sie die Stange (langsam und kontrolliert) an, bis Sie Ihre Ellbogen gestreckt haben.</p>",
+                //},
+              }
+            ],
+            //,
+          
+
+            // 
+            //{
+            name: "Pull",
+            exercises: [
+              {
+                
+                  name: "Kreuzheben",
+                  image: "Musterbild",
+                  difficulty: "Fortgeschritten",
+                  muscleGroup: "Rücken",
+                  description: "<p>1. Stellen Sie sich mit dem Mittelfuß unter die Stange und greifen Sie die Stange mit den Händen etwa schulterbreit auseinander.</p><p>2. Beugen Sie Ihre Knie und heben Sie dann die Stange an, indem Sie Ihren Rücken strecken. Es ist wichtig, den Rücken gerade zu halten.</p><p>3. Stehen Sie zu Ihrer vollen Höhe und halten Sie.</p><p>4.Senken Sie die Stange auf den Boden, indem Sie Ihre Knie beugen und Ihren Rücken gerade halten.</p>",
+                },
+                {
+                  name: "Langhantelrudern vorgebeugt",
+                  image: "Musterbild",
+                  difficulty: "Mittel",
+                  muscleGroup: "Rücken",
+                  description: "<p>1. Greifen Sie eine Langhantel mit einem schulterbreiten pronierten oder supinierten Griff.</p><p>2. Beuge dich an deinen Hüften nach vorne, während du einen flachen Rücken behältst.</p><p>3. Ziehen Sie das Gewicht in Richtung Oberbauch.</p><p>4. Senken Sie das Gewicht kontrolliert ab und wiederholen Sie den Vorgang.</p>",
+                
+              }
+            ],
+            //}
+
+            // // Legs
+            // {
+            name: "Legs",
+            exercises: [
+              {
+              name: "Squat",
               image: "Musterbild",
-              difficulty: "schwer",
-              muscleGroup: "Rücken",
-              description: "Ziehe eine Langhantel.",
-            },
-          ],
-        },
+              difficulty: "Fortgeschritten",
+              muscleGroup: "Beine",
+              description: "<p>1. Stellen Sie sich mit den Füßen schulterbreit auseinander. Behalten Sie die natürliche Wölbung in Ihrem Rücken bei, drücken Sie Ihre Schulterblätter zusammen und heben Sie Ihre Brust an.</p><p>2. Greifen Sie die Stange über Ihre Schultern und stützen Sie sie auf Ihrem oberen Rücken ab. Lösen Sie die Stange, indem Sie Ihre Beine strecken, und treten Sie einen Schritt zurück.</p><p>3. Beugen Sie Ihre Knie, während Sie das Gewicht senken, ohne die Form Ihres Rückens zu verändern, bis sich Ihre Hüften unter Ihren Knien befinden.</p><p>4. Heben Sie die Stange zurück in die Ausgangsposition, heben Sie sie mit den Beinen an und atmen Sie oben aus.</p>",
+              },
+              {
+                name: "Wadenheben im Sitzen",
+                image: "Musterbild",
+                difficulty: "Leicht",
+                muscleGroup: "Beine",
+                description: "<p>1. Machen Sie es sich auf der Maschine bequem und legen Sie dann Ihre Unterschenkel unter den gepolsterten Hebel. Stellen Sie Ihre Zehen und Fußballen auf die Fußstützen.</p><p>2. Verhindern Sie, dass das Gewicht nach vorne rutscht, indem Sie die Griffe greifen, und lösen Sie den Sicherheitsbügel. Senken Sie das Gewicht, bis Ihre Waden gestreckt sind.</p><p>3.Drücken Sie Ihre Fersen nach oben, um den gepolsterten Hebel anzuheben und die kontrahierte Position zu halten, und senken Sie sich dann langsam wieder in die Ausgangsposition ab. Wiederholen.</p>",
+              },
+              {
+                name: "Ausfallschritt Kettlebell",
+                image: "Musterbild",
+                difficulty: "Leicht",
+                muscleGroup: "Beine",
+                description: "<p>1. Stellen Sie sich aufrecht hin, die Füße leicht auseinander und halten Sie eine Kettlebell in einer Hand.</p><p>2. Bringen Sie dasselbe Bein wie den Arm, der die Kettlebell hält, vor sich, gehen Sie in die Hocke, bis Ihr Oberschenkel parallel zum Boden ist, und halten Sie Ihren Rücken gerade.</p><p>3. Zurück in die Ausgangsposition und wiederholen.</p>",
+              },
+              {
+                name: "Beinpresse",
+                image: "Musterbild",
+                difficulty: "Mittel",
+                muscleGroup: "Beine",
+                description: "<p>1. Stellen Sie Ihre Beine schulterbreit auf die Plattform.</p><p>2. Lassen Sie das Gewicht los und strecken Sie Ihre Beine vollständig aus, ohne Ihre Knie zu sperren.</p><p>3. Senken Sie das Gewicht ab, bis sich Ihre Beine in einem 90°-Winkel befinden (aber lassen Sie NICHT zu, dass sich Ihr Gesäß und Ihr unterer Rücken vom Polster abheben. Dadurch wird Ihr unterer Rücken in eine runde Position gebracht, was sehr gefährlich ist.)</p><p>4. Drücken Sie das Gewicht zurück in die Ausgangsposition.</p>",
+              }
+            ],
+          },
+          
       ]);
     }
   }
